962ef6272ba7343161a50f3ae2299e6d6b27fcfa
Harsh Verma <av15204050@gmail.com>
2026-09-15 11:25:23 +0530

    Footed Added

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import BookingProcess from "./sections/BookingProcess";
 import Timing from "./sections/Timing";
 import TestimonialSection from "./sections/TestimonialSection";
 import FAQs from "./sections/FAQs";
+import Footer from "./sections/Footer";
 
 const App = () => {
   return (
@@ -23,6 +24,7 @@ const App = () => {
         <Timing/>
         <TestimonialSection/>
         <FAQs/>
+        <Footer/>
      </>
   );
 };
